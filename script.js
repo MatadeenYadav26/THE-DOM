8976b90168288fbb5d85c9db6ab827551dd20b33
KING <matadeenyadav202@gmail.com>
2026-02-13 20:51:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//error
+// var h1 = document.querySelector("h1");
+// console.log("Found:", h1);
+
+// // HTML SELECTOR :
+// var h1 = document.querySelector("h1")
+// var h2 = document.querySelector("h2")
+// console.log(h1);
+// console.log(h2);
+// //HTML CHANGE :
+// h2.innerHTML='Hello Bachho'
+// h1.innerHTML='Hello Girls'
+// // CSS CHANGE : 
+// h2.style.color="Yellow";
+// h1.style.backgroundColor="grey";
+
+//rvsn:
+// var rvsn = document.querySelector("#box")
+// box.innerHTML="Heyy!"
+// box.style.backgroundColor= "Pink";
+
+// Event Listners :
+// var h1 = document.querySelector("h1");
+// h1.innerHTML="Changed";
+// h1.style.color="Gold";
+
+// h1.addEventListener('click',function(){
+//     console.log("Click Kiya");
+// })
+
+
+// click issue :
+// document.querySelector("h1").addEventListener("click", () => console.log("works"));
+// h1.onclick = function () {
+//     alert("Clicked!");
+// };
+
+// h1.addEventListener("click", () => {
+//     console.log("%cClicked!", "color: gold; font-size:16px;");
+// });
+
+// Event Listners dbl-click:
+// var h1 = document.querySelector("h1");
+// h1.innerHTML="Changed";
+// h1.style.color="Gold";
+
+// h1.addEventListener('dblclick',function(){
+//     console.log("Click Kiya");
+// })
