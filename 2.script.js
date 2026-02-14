cc44dfe9c5efc84dded7a434d77e4e196f012599
KING <matadeenyadav202@gmail.com>
2026-02-14 12:49:51 +0530

    Add files via upload

---
+++ 2.script.js
@@ -0,0 +1,49 @@
+// var h1 = document.querySelector("h1")
+// var btn = document.querySelector('button')
+
+// btn.addEventListener('click',function(){
+//     h1.innerHTML="I AM BATMAN";
+//     h1.style.color="Red";
+//     h1.style.fontSize="60px";
+// })
+
+// methods to select html Elements:
+// 1: document.querySelector(#hero);
+// 2: Selection by id:
+// var h1 = document.getElementById('hero')
+// console.log(h1);
+// 3: Selection by class:
+// var h1 = document.getElementsByClassName('elem')
+// console.log(h1);
+
+// 4 Pillars of DOM :
+//                  1.Selection:
+//                   - querySelector
+//                   - getElementById
+//                   - getElementByClassname
+//                   - querySelectorAll
+//                  2.Changing HTML
+//                   -innerHTML=''
+//                   -textContent=''
+//                  3.Changing CSS
+//                  4.Event listeners
+
+// 1.4: querySelectorAll:
+
+// var h1 = document.querySelector("h1");
+// h1.innerHTML="Changed";
+
+// to change all the h1 elements all at once not only 1st one : 
+
+// var h1 = document.querySelectorAll("h1");
+// h1.innerHTML="Changed";
+
+// console.log(h1);
+
+// var h1 = document.querySelector('h1');
+
+// h1.innerHTML="Hello";
+// h1.textContent="Helloooo";
+// h1.textContent="<b>Helloooo</b>";
+// h1.innerHTML="<i>Helloooo</i>";
+
