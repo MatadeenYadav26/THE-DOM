d35d73272057d2113f47f8d77bc4a2528aac61bc
KING <matadeenyadav202@gmail.com>
2026-03-13 07:00:38 +0530

    Add files via upload

---
+++ Day-49 DOM AGAIN/script.js
@@ -0,0 +1,34 @@
+// var h1 = document.querySelectorAll('h1')
+
+// // console.log(Allh1);
+
+// // Allh1.forEach(function(elem){
+// //     console.log(elem.innerHTML );
+    
+// // })
+
+// console.log(h1[0].innerHTML);
+// // console.log(h1[1].innerHTML);
+// // console.log(h1[2].innerHTML);
+// // console.log(h1[3].innerHTML);
+// h1[1].innerHTML="Bye-Bye"
+
+
+// var outer = document.querySelector('#outer')
+// console.log(outer.childNodes);
+
+
+var Allelem = document.querySelectorAll('.elem')
+
+// console.log(Allelem);
+
+Allelem.forEach(function(elem){
+    // console.log(elem);
+    // console.log(elem.innerHTML);
+    // console.log(elem.childNodes);
+    // console.log(elem.childNodes[0]);
+    // console.log(elem.childNodes[1]);
+    // console.log(elem.childNodes[2]);
+    // console.log(elem.childNodes[3]);
+    
+})
