8bb0f9a192a87e3a9a41a45ac0423b4ddc2ab1df
KING <matadeenyadav202@gmail.com>
2026-03-17 06:16:41 +0530

    Add files via upload

---
+++ Day-50 DOM/script.js
@@ -0,0 +1,64 @@
+// var allElems = document.querySelectorAll('.elem')
+
+// allElems.forEach(function(elem) {
+//     // console.log(elem.childNodes);
+//     // console.log(elem.childNodes[0]);
+//     // console.log(elem.childNodes[1]);
+//     // console.log(elem.childNodes[2]);
+//     // console.log(elem.childNodes[3]);
+
+//     elem.childNodes[3].addEventListener('click',function(){
+//         console.log('Hey!');
+//     })
+    
+// });
+
+
+
+
+
+// var allbtn = document.querySelectorAll('button')
+
+// allbtn.forEach(function(elem){
+//     elem.addEventListener('click',function(){
+//         // console.log("Clicked");
+//         // elem.innerHTML='Remove Friend'
+//         if(elem.innerHTML=='Add Friend'){
+//             elem.innerHTML='Remove Friend'
+//         }
+//         else{
+//             elem.innerHTML='Add Friend'
+//         }
+        
+//     })
+// })
+
+
+
+
+//Use of map : for tranformation , foreach : used for itteration
+
+
+// var arr = [10,20,30,40]
+
+// var aaarr = arr.forEach(function(elem){
+//     // console.log('Hello',elem);
+//     return(elem)
+// })
+// console.log(aaarr);
+
+
+// var mapp  = arr.map(function(elem){
+//     // console.log('Hello',elem);
+//     return(elem*elem)
+// })
+// console.log(mapp);
+
+// both give same O/p in console are they both same ? No , bcoz for-each returns nothing if stored in a variable but map does. 
+// so for-each is used for itteration , map is used for tranformation, filter is used for filteration.
+
+
+
+
+
+
